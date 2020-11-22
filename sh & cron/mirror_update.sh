0663fa323fc00a6ba8a595efea73e6eabadd0151
Kwabang <kwabang2827@gmail.com>
2020-11-22 19:49:48 +0900

    Update index & synchronization script

--- sh & cron/mirror_update.sh
+++ sh & cron/mirror_update.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-cp /root/update/page/offline.html /etc/nginx/html/index.html
-cp /root/update/offline.conf /etc/nginx/nginx.conf
-/usr/sbin/nginx -s reload
-rm -rf /HDD/archive/skel/
-apt-mirror
-rm -rf /HDD/archive/mirror/ports.ubuntu.com/ubuntu-ports/dists/focal*/*/cnf
-rm -rf /HDD/archive/mirror/archive.ubuntu.com/ubuntu/dists/focal*/*/cnf
+apt-mirror #Update Package File
 cd /HDD/archive/mirror/
-./archive.ubuntu.com.cnf.sh
-./ports.ubuntu.com.cnf.sh
-cd /HDD/archive/mirror/ports.ubuntu.com/ubuntu-ports
-rm -f ls-lR.gz
-ls -lR > ls-lR
-gzip ls-lR
-cd /HDD/archive/mirror/archive.ubuntu.com/ubuntu
-rm -f ls-lR.gz
-ls -lR > ls-lR
-gzip ls-lR
-cp /root/update/page/online.html /etc/nginx/html/index.html
-cp /root/update/online.conf /etc/nginx/nginx.conf
-/usr/sbin/nginx -s reload
+./archive.ubuntu.com.cnf.sh & #Update Ubuntu cnf
+./ports.ubuntu.com.cnf.sh #Update Ubuntu-Ports cnf
+fg #check job
+cd /HDD/archive/mirror/archive.ubuntu.com/ubuntu/
+ls -lR > /root/update/tmp/ls-lR #Indexing Ubuntu
+gzip /root/update/tmp/ls-lR #compression
+mv /root/update/tmp/ls-lR.gz /HDD/archive/mirror/archive.ubuntu.com/ubuntu/ls-lR.gz #Update Index File
+cd /HDD/archive/mirror/ports.ubuntu.com/ubuntu-ports/
+ls -lR > /root/update/tmp/ls-lR #Indexing Ubuntu-ports
+gzip /root/update/tmp/ls-lR #compression
+mv /root/update/tmp/ls-lR.gz /HDD/archive/mirror/ports.ubuntu.com/ubuntu-ports/ls-lR.gz #Update Index File
